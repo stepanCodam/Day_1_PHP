d1d38e2e8db418c84c7115b68256f78303945051
Stefan <stefanteaca37@gmail.com>
2022-03-14 11:44:43 +0100

    check all

---
+++ basic/ex2.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $name = "Bob murder";
+    $age = 23;
+    $jobTitle = "Janitor";
+    
+    ?>
+
+    <h1>HI, my name is
+    <?php
+    echo $name;
+    ?>
+    ,I am 
+    <?php
+    echo $age;
+    ?>
+    years old and I work as a <?php
+    echo $jobTitle
+    ?>
+    
+    
+</h1>
+</body>
+</html>
---
+++ basic/ex3.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+    $names = array("John","Mark","Georg","Lisa")
+
+    ?>
+    <h3>The third player in the team is
+    <?php
+      echo  $names[2];
+    ?>
+</h3>
+</body>
+</html>
